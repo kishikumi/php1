b5ddf379d99c2c97d2e98971653841979cd87b13
kishikumi <shiokumi0520@gmail.com>
2024-12-12 18:12:50 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>フォームアプリ</title>
     <style>
-        /* 優しいタッチのスタイル */
         body {
             font-family: Arial, sans-serif;
             background-color: #f9f9f9;
